ec08bd726bcfd1b8c237c0e1d3d3604b61d2c71a
Ken Cruz <ken@kencruz.ca>
2021-11-09 16:14:05 -0500

    implemented log out

--- express_server.js
+++ express_server.js
@@ -28,6 +28,11 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
