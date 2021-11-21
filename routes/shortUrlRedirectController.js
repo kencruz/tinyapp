6cdf13fe63335ed5a58032098bea06f06a948406
Ken Cruz <ken@kencruz.ca>
2021-11-21 17:59:30 -0500

    refactored shortUrl redirects to a controller

---
+++ routes/shortUrlRedirectController.js
@@ -0,0 +1,37 @@
+const { users, urlDatabase } = require('../data');
+const {
+  generateRandomString,
+} = require('../helpers');
+
+const redirectShortUrl = (req, res) => {
+  // get shortURL from request body and try to get a valid user id from cookie
+  const [shortURL, user] = [req.params.shortURL, users[req.session.userId]];
+
+  // invalid shortURLs are shown the error page
+  if (!urlDatabase[shortURL]) {
+    return res
+      .status(404)
+      .render('error', { user, error: 'Short link not found' });
+  }
+
+  // if no visitorId, create one
+  if (!req.session.visitorId) {
+    req.session.visitorId = generateRandomString();
+  }
+  const visitorId = req.session.visitorId;
+
+  // once successfully validated, redirect to longURL
+  const longURL = urlDatabase[shortURL].longURL;
+  // increment visitor counters
+  if (!urlDatabase[shortURL].visitors[visitorId]) {
+    urlDatabase[shortURL].visitors[visitorId] = 0;
+  }
+  urlDatabase[shortURL].visitors[visitorId]++;
+  // Track the visit
+  urlDatabase[shortURL].trackingLog.push({timestamp: new Date(), visitorId});
+  return res.redirect(longURL);
+};
+
+module.export = {
+  redirectShortUrl
+};
